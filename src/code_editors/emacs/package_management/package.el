d074570270fe21922a888caf8cd244e79e98464e
Roman Frołow <rofrol@gmail.com>
2018-06-07 01:08:10 +0200

    bump

---
+++ src/code_editors/emacs/package_management/package.el
@@ -0,0 +1,7 @@
+call package-initialize with non-nil optional argument, e.g.,
+
+`(package-initialize 'no-activate)`
+
+This is how I have always use package.el so that I can have many installed packages, but activate only some of them by using a different init file when emacs starts.
+
+https://github.com/raxod502/straight.el/issues/128#issuecomment-323602566
