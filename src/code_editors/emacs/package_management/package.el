ee7e2b36552c2467fa8d621d333a16ade6db8a9c
Roman Frołow <rofrol@gmail.com>
2018-06-07 01:47:41 +0200

    bump

--- src/code_editors/emacs/package_management/package.el
+++ src/code_editors/emacs/package_management/package.el
@@ -13,10 +13,6 @@ List of packages
 - http://melpa.milkbox.net/#/getting-started
 - http://ergoemacs.org/emacs/emacs_package_system.html
 
-## Speed
-
-- https://www.reddit.com/r/emacs/comments/87g6yg/new_packagequickstart_feature_in_emacs_27/
-
 ## package-initialize
 
 call package-initialize with non-nil optional argument, e.g.,
@@ -32,6 +28,18 @@ or?
 `(setq package-enable-at-startup nil)`
 
 https://github.com/raxod502/straight.el/issues/128#issuecomment-325248034
+
+Emacs-27
+
+Installed packages are now activated *before* loading the init file.
+This is part of a change intended to eliminate the behavior of
+package.el inserting a call to 'package-initialize' into the init
+file, which was previously done when Emacs was started.  As a result
+of this change, it is no longer necessary to call 'package-initialize'
+in your init file.
+
+- https://github.com/emacs-mirror/emacs/blob/master/etc/NEWS
+- https://www.reddit.com/r/emacs/comments/87g6yg/new_packagequickstart_feature_in_emacs_27/
 
 ## package-alist
 
