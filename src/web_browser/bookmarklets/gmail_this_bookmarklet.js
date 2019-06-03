13177cf759b0f8274dc6b5c2af832c71bbcd1bb7
Roman Frołow <rofrol@gmail.com>
2019-06-03 12:34:15 +0200

    Make selecting text work

--- src/web_browser/bookmarklets/gmail_this_bookmarklet.js
+++ src/web_browser/bookmarklets/gmail_this_bookmarklet.js
@@ -3,19 +3,8 @@
 // https://github.com/KartikTalwar/gmail.js
 //javascript:
 (function(){
-    popw='';
-    Q='';
-    d=document;
-    w=window;
-    if(d.selection){
-	Q=d.selection.createRange().text;
-    } else if(w.getSelection){
-	Q=w.getSelection();
-    } else if(d.getSelection){
-	Q=d.getSelection();
-    }
-    var email = '';
-
+    email='';popw='';Q='';d=document;w=window;
+    if(d.selection){Q=d.selection.createRange().text;}else if(w.getSelection){Q=w.getSelection();}else if(d.getSelection){Q=d.getSelection();}
     popw=w.open('http://mail.google.com/mail/s?view=cm&fs=1&tf=1&to='+email+'&su='+encodeURIComponent(d.title)+'&body='+encodeURIComponent(Q)+escape('%5Cn%5Cn')+encodeURIComponent(d.location)+'&zx=RANDOMCRAP&shva=1&disablechatbrowsercheck=1&ui=1','_blank');
-
+    if(!d.all)setTimeout(function(){popw.focus();},50);
 })();
