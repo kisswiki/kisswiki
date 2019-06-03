97f3d79898f3cae38f46b06cb7ece2db1f912641
Roman Frołow <rofrol@gmail.com>
2019-06-03 07:25:42 +0200

    bump

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ln -sf $PWD/src/bin/ ~/
-ln -sf $PWD/src/dotfiles/.bashrc ~/
-ln -sf $PWD/src/dotfiles/sync-history.sh ~/
-ln -sf $PWD/src/dotfiles/.tmux.conf ~/
-mkdir -p ~/.config/mpv/
-ln -sf $PWD/src/dotfiles/.config/mpv/* ~/.config/mpv/
