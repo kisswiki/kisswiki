f2f52e38637f50b698393e44b83bce57e7f50eae
Roman Frołow <rofrol@gmail.com>
2019-06-16 10:28:05 +0200

    bump

--- src/web_browser/bookmarklets/rarbg_imdb.js
+++ src/web_browser/bookmarklets/rarbg_imdb.js
@@ -4,10 +4,15 @@
     var r = /IMDB: (\d+(\.\d+)?)\/10/;
     var list = document.querySelectorAll('.lista2t .lista2 .lista:nth-of-type(2)');
     for (var el of list) {
-        var num = parseFloat(r.exec(el.innerText)[1]);
-        if(num < 7) {
-            var row = el.closest('.lista2');
-            row.parentNode.removeChild(row);
-        }
+	var row = el.closest('.lista2');
+	var result = r.exec(el.innerText);
+	if(result) {
+	    var num = parseFloat(r.exec(el.innerText)[1]);
+	    if(num < 7) {
+		row.remove();
+	    }
+	} else {
+	    row.remove();
+	}
     }
 })();
