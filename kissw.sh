43f0f0cd1bf4f5f5fee4b8fc2509f10145b492b3
Roman Frołow <rofrol@gmail.com>
2020-08-29 11:47:08 +0200

    kissw.sh

--- kissw.sh
+++ kissw.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-git add -A && git commit -a -m 'bump'; git pull --rebase; git push
+# https://unix.stackexchange.com/questions/114244/replace-all-newlines-to-space-except-the-last/114281#114281
+git add -A && git commit -a -m "$(git diff --name-only --staged | perl -p00e 's/\n(?!\Z)/, /g')"; git pull --rebase; git push
