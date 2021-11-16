c82d72cd1ca9efbaf71d74a369f6c278d1ad4531
Roman Frołow <rofrol@gmail.com>
2021-11-16 16:48:15 +0100

    src/os/nixos/gnome.nix

--- src/os/nixos/gnome.nix
+++ src/os/nixos/gnome.nix
@@ -5,3 +5,13 @@ There are many sounds when there is i.e. installation of mongodb and there are s
 `gsettings set org.gnome.desktop.sound event-sounds false`
 
 https://unix.stackexchange.com/questions/444681/how-to-turn-off-alert-sounds-sound-effects-on-gnome-from-terminal#comment1069287_444869
+
+## Extensions
+
+Open Extensions app and enable extensions with global switch.
+
+Then enable specific extensions. You may need to relogin or reboot to enable extensions switch.
+
+The one installed from nix maybe too old for current gnome. Then install from https://extensions.gnome.org.
+
+https://discourse.nixos.org/t/gnome-extensions-updates-and-testing/16088/2
