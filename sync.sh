eba655887c61ff8a00b4865956b03d5b6b6412fc
Roman Frołow <rofrol@gmail.com>
2022-04-09 08:23:54 +0200

    sync.sh

--- sync.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# https://unix.stackexchange.com/questions/114244/replace-all-newlines-to-space-except-the-last/114281#114281
-git add -A && git commit -a -m "$(git diff --name-only --staged | perl -p00e 's/\n(?!\Z)/, /g')"; git pull --rebase; git push
