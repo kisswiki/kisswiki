c234dc17a93e46ff44439c2437c5ad8823169b1e
Roman Frołow <rofrol@gmail.com>
2024-01-06 00:58:09 +0100

    src/web_browser/bookmarklets/zap_colors.js

---
+++ src/web_browser/bookmarklets/zap_colors.js
@@ -0,0 +1,3 @@
+// https://www.squarefree.com/bookmarklets/zap.html
+//javascript:
+(function(){var newSS, styles='* { background: white !important; color: black !important;  text-shadow: unset !important; font-weight: normal !important; } :not(code) { font-family: sans-serif !important; } :link, :link * { color: #0000EE%20!important%20}%20:visited,%20:visited%20*%20{%20color:%20#551A8B%20!important%20}';%20if(document.createStyleSheet)%20{%20document.createStyleSheet("javascript:'"+styles+"'");%20}%20else%20{%20newSS=document.createElement('link');%20newSS.rel='stylesheet';%20newSS.href='data:text/css,'+escape(styles);%20document.getElementsByTagName("head")[0].appendChild(newSS);%20}%20})();
