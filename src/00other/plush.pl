2a4cda5c24c251e0a39c7aae1d03871553759a61
Roman Frołow <rofrol@gmail.com>
2024-07-08 15:24:12 +0200

    src/00other/plush.pl

---
+++ src/00other/plush.pl
@@ -0,0 +1,30 @@
+## Wypowiedzenie umowy
+
+Początkowo należy wypowiedzieć umowę w ofercie abonamentowej, następnie przed wskazaną datą dezaktywacji wysyłać bezpłatnego SMS-a o treści PRZENIES na numer 80107. Wszystkie informacje tutaj: https://bok.plus.pl/article/448
+
+Cesji można dokonać w salonie Plusa
+
+https://www.plus.pl/znajdz-salon
+
+instrukcja wypowiedzenia:
+
+Pismo można złożyć w każdym punkcie Plusa lub przesłać listem poleconym na adres:
+
+Polkomtel Sp. z o.o.
+
+ul. Konstruktorska 4
+
+02-673 Warszawa
+
+z dopiskiem 'Rezygnacja z usług'.
+
+Wniosek powinien zawierać:
+
+- informację, iż umowa nie będzie przedłużana po czasie oznaczonym,
+- imię i nazwisko,
+- numer telefonu, którego dotyczy sprawa,
+- czytelny podpis właściciela.
+
+Istnieje możliwość przesłania wypowiedzenia drogą elektroniczną. W tym celu należy wysłać wniosek z adresu mailowego, na który kierowane są faktury (adres do Zielonej faktury) lub z adresu podanego przy zawieraniu umowy w formie dokumentowej.
+
+Adres do wysyłki to bok@plus.pl. W treści należy podać PK5 lub numer PESEL, ewentualnie dodać skan wniosku z odręcznym podpisem dołączonym w formie załącznika.
